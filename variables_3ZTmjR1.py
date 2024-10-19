85069c457071c0e20d824dc67f5d5c6ca8f180f6
NikolayKydrushov <163329496+NikolayKydrushov@users.noreply.github.com>
2024-10-19 18:13:40 +0300

    Add files via upload

---
+++ variables_3ZTmjR1.py
@@ -0,0 +1,8 @@
+number_of_completed_DZ = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent/number_of_completed_DZ
+print('Курс:', course_name)
+print ('Всего задач:', number_of_completed_DZ)
+print ('Затрачено часов', number_of_hours_spent)
+print ('Среднее время выполнение', time_for_one_task, 'часов')

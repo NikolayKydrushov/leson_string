058f08d33f0a942515b6b4048f3dceb4e3bb05cd
NikolayKydrushov <163329496+NikolayKydrushov@users.noreply.github.com>
2024-10-26 10:53:36 +0300

    Update variables_3ZTmjR1.py

--- variables_3ZTmjR1.py
+++ variables_3ZTmjR1.py
@@ -2,7 +2,4 @@ number_of_completed_DZ = 12
 number_of_hours_spent = 1.5
 course_name = 'Python'
 time_for_one_task = number_of_hours_spent/number_of_completed_DZ
-print('Курс:', course_name)
-print ('Всего задач:', number_of_completed_DZ)
-print ('Затрачено часов', number_of_hours_spent)
-print ('Среднее время выполнение', time_for_one_task, 'часов')
+print('Курс:', course_name,', всего задач:', number_of_completed_DZ, ', затрачено часов', number_of_hours_spent, ', среднее время выполнение', time_for_one_task, 'часов')
